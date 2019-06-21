16adbb0da096f88ed8881e62697dabb251668ecb
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-06-21 14:43:04 -0400

    corrected the error to get the webpage to work smoothly

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -15,7 +15,7 @@ class TabLink {
 
         } else {
             //     // else if `all` is false, only select the cards with matching this.tabData values
-            this.cards = document.querySelectorAll(`.card[data-tab="${this.data}"]`);
+            this.cards = document.querySelectorAll(`.card[data-tab='${this.tabData}']`);
         }
 
         // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
@@ -34,7 +34,7 @@ class TabLink {
         console.log(tabs)
             // Iterate through the NodeList removing the .active-tab class from each element
         tabs.forEach(tab => {
-            tab.classList.remove('.active-tab');
+            tab.classList.remove('active-tab');
         });
 
         // Select all of the elements with the .card class on them
@@ -46,7 +46,7 @@ class TabLink {
         });
 
         // Add a class of ".active-tab" to this.tabElement
-        this.tabElement.classList.add('.active-tab');
+        this.tabElement.classList.add('active-tab');
 
         // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
         this.cards.forEach(card => card.selectCard());
